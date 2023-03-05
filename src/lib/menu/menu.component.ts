0e05161b71a6c7307291ebb9c378763579e92418
AlexStorm <info@alexstorm.ru>
2023-03-05 17:50:08 +0530

    fix menu

--- src/lib/menu/menu.component.ts
+++ src/lib/menu/menu.component.ts
@@ -3,34 +3,34 @@ import {NavigationEnd, Router} from "@angular/router";
 import {MenuItemData} from "../model";
 
 @Component({
-  selector: "ui-menu",
-  templateUrl: "./menu.component.html",
-  styleUrls: ["./menu.component.css"]
+    selector: "ui-menu",
+    templateUrl: "./menu.component.html",
+    styleUrls: ["./menu.component.css"]
 })
 export class MenuComponent implements OnInit {
 
-  @Input()
-  data!: MenuItemData[];
+    @Input()
+    data!: MenuItemData[];
 
-  @Input()
-  forModule = true;
+    @Input()
+    forModule = true;
 
 
-  module!: string;
+    current!: string;
 
-  constructor(private router: Router) {
-    this.router.events.subscribe((event) => {
+    constructor(private router: Router) {
+        this.router.events.subscribe((event) => {
 
       if (event instanceof NavigationEnd) {
-        this.module = event.url.split("/")[1];
-        console.log("Navigation End", this.module);
+        this.current = event.url;
+        console.log("Navigation End", this.current);
         console.log("data", this.data);
       }
     });
   }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
 
